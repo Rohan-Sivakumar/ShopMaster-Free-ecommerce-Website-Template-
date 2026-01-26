f5bc277f5e61835150b5b4e36a83adf7a81882aa
Rohan <rohan.sivaa@gmail.com>
2026-01-26 08:17:03 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1033,7 +1033,7 @@ function App() {
           <h4 className="fw-bold mb-1">{currentUser?.name}</h4>
           <p className="text-muted small mb-4">{currentUser?.email}</p>
           <button className="btn btn-outline-danger btn-sm px-4 rounded-pill" onClick={handleSignOut}>
-            Sign Out
+          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#EA3323"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h280v80H200Zm440-160-55-58 102-102H360v-80h327L585-622l55-58 200 200-200 200Z"/></svg> Sign Out
           </button>
         </div>
       </div>
